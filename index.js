35351516b2d71eacfc5d21f3304b1ef5150e84c1
Vitor Pouza <vhnpouza@gmail.com>
2024-05-07 17:54:54 -0300

    oauth

--- index.js
+++ index.js
@@ -1,9 +1,25 @@
 const express = require('express');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
+const mongoose = require('mongoose');
+const keys = require('./config/keys');
+require('./models/user');
+require('./services/passport');
+
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send({hi: "there"})
-})
+app.use(
+    cookieSession({
+        maxAge: 30 * 24 * 60 * 60 * 1000,
+        keys: [keys.cookieKey]
+    })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+
+mongoose.connect(keys.mongoURI);
+
+require('./routes/authRoutes')(app);
 
 const PORT = process.env.PORT || 5000;
 
