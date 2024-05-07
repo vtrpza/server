c16ccb96c22c07942484167e4c03024afbee69f1
Vitor Pouza <vhnpouza@gmail.com>
2024-05-07 19:01:00 -0300

    created client

---
+++ client/src/components/App.js
@@ -0,0 +1,9 @@
+const App = () => {
+    return (
+        <div>
+            Hi there!
+        </div>
+    );
+};
+
+export default App;
---
+++ client/src/index.js
@@ -0,0 +1,17 @@
+import ReactDOM from "react-dom/client";
+import App from './components/App';
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from 'redux';
+
+const store = createStore(() => [], {}, applyMiddleware());
+
+const el = document.getElementById("root");
+
+const root = ReactDOM.createRoot(el);
+
+root.render(
+    <Provider store={store}>
+        <App />
+    </Provider>
+);
+
