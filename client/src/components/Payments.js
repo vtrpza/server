8d190085b17bf43a165a5452100fc97eb1d82598
Vitor Pouza <vhnpouza@gmail.com>
2024-05-08 15:24:39 -0300

    add stripe

---
+++ client/src/components/Payments.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+
+class Payments extends Component {
+    render() {
+        return (
+            <StripeCheckout
+                name='Azapy'
+                description='$5 for 5 email credits'
+                amount={500}
+                token={(token) => {
+                    console.log(token)
+                }}
+                stripeKey={process.env.REACT_APP_STRIPE_KEY}
+            >
+                <button className='btn'>
+                    Add credits
+                </button>
+            </StripeCheckout>
+        );
+    }
+}
+
+export default Payments;
