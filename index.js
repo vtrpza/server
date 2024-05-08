a2bd9b73df6576d3f6f72f30d89b5276f6bc1bed
Vitor Pouza <vhnpouza@gmail.com>
2024-05-08 15:59:22 -0300

    billing

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cookieSession = require('cookie-session');
 const passport = require('passport');
+const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const keys = require('./config/keys');
 require('./models/user');
@@ -8,6 +9,7 @@ require('./services/passport');
 
 const app = express();
 
+app.use(bodyParser.json());
 app.use(
     cookieSession({
         maxAge: 30 * 24 * 60 * 60 * 1000,
@@ -20,6 +22,7 @@ app.use(passport.session());
 mongoose.connect(keys.mongoURI);
 
 require('./routes/authRoutes')(app);
+require('./routes/billingRoutes')(app);
 
 const PORT = process.env.PORT || 5000;
 
